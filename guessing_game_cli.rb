152ec52e3110f94d4fbd60d902c215cdfd587067
Mohammed shaikh <mqshaikh8@gmail.com>
2019-11-06 14:55:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,6 +3,7 @@ def run_guessing_game
 	roll = 1+rand(6)
 	user_input = gets.chomp.to_s
   if user_input == roll
-    puts "You "
+    puts "You guesses the coorect number!"
+  elsif user_input != roll
   
 end 
